9bd4cc4948f27ea48924eaf5712c7190a913ee78
Bharath Sriraam R R <albharath.1305@gmail.com>
2023-06-22 19:25:37 +0530

    bla

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,8 @@
 
 import "./pdf";
 
-// import Draw from "@/app/tldraw";
-
-import dynamic from "next/dynamic";
-
-const Editor = dynamic(async () => import("./tldraw"), { ssr: false });
+import Draw from "@/app/reactFlow";
 
 export default function Home() {
-  return <Editor />;
+  return <Draw />;
 }
